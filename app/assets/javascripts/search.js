48a37c27562311dc77551a7143cc7b587ed33cdf
codebicycle <codebicycle@gmail.com>
2016-10-21 16:28:37 +0300

    Photos search pagination

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,5 +1,21 @@
-$(document).ready(function() {
+$(document).ready(function () {
+  toggle_form();
+  pagination();
+});
+
+let toggle_form = function () {
   $("#toggle-form-btn").click(function () {
     $('#Main').toggleClass('hide');
   });
-});
+};
+
+let pagination = function () {
+  $('.pagination-link').click( function(e) {
+    e.preventDefault();
+    let form_id = e.target.dataset.form
+    let page = e.target.dataset.param_page
+    $('#param_page').val(page)
+    $('#'+form_id).submit()
+    return false;
+  });
+};
--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -7,8 +7,24 @@ class SearchController < ApplicationController
                     .map { |k,v| [k.sub('param_', '').to_sym, v] }
                     .to_h
 
-      @photos = flickr.photos.search(mod_params)
+      photos = flickr.photos.search(mod_params)
+      pagination = gen_page_numbers(current_page: photos.page,
+                                      max_page: photos.pages)
+      @out = {photos: photos,
+              pagination: pagination}
     end
+  end
+
+private
+  def gen_page_numbers(current_page:, max_page:)
+    pagination = {}
+    pagination[:current] = current_page
+    pagination[:pages] = []
+    pagination[:pages] += (current_page-5..current_page).select { |p| p > 0 }
+    pagination[:pages] += (current_page+1..current_page+5)
+                            .select {|p| p < max_page + 1 }
+
+    pagination
   end
 
 end
