428e204a7dd12523bc41e8c0664c09ee68952ba5
codebicycle <codebicycle@gmail.com>
2016-10-21 15:41:12 +0300

    Photos search first draft

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,14 @@
 class SearchController < ApplicationController
 
   def index
+    if request.post?
+      mod_params = params
+                    .select { |k,v| k.start_with?('param') and !v.blank? }
+                    .map { |k,v| [k.sub('param_', '').to_sym, v] }
+                    .to_h
+
+      @photos = flickr.photos.search(mod_params)
+    end
   end
+
 end
---
+++ config/initializers/flickr-keys.rb
@@ -0,0 +1,4 @@
+FlickRaw.api_key =      Rails.application.secrets.flickr_key
+FlickRaw.shared_secret= Rails.application.secrets.flickr_secret
+# flickr.access_token = 
+# flickr.access_secret =
